9b8a2e6d0c3c7817b10806cbc8a6824f3db0e523
Nabil <hayounn.etd@gmail.com>
2023-02-12 15:52:46 +0100

    Pass city name as prop to parent compoenent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, { Component } from 'react';
 import SearchBox from './components/search-input/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBox />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const apiKey = '190a729e56399683fb4a81428b63841f';
+    const { cityName } = this.state;
+    if (prevState.cityName !== cityName && cityName !== '') {
+      fetch()
+    }
+  }
+
+  handleDataChange = (data) => {
+    this.setState({
+      cityName: data,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBox onSearchChange={handleDataChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/search-input/search-box.component.jsx
+++ src/components/search-input/search-box.component.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 class SearchBox extends Component {
   constructor() {
@@ -6,17 +8,33 @@ class SearchBox extends Component {
     this.state = {
       searchField: '',
     };
+    this.onSearchChanged = this.onSearchChange.bind(this);
   }
 
-  render() {
+  onSearchChange = (e) => {
+    const { value } = e.target;
     const { searchField } = this.state;
+    const { onSearchChange } = this.props;
+    this.setState({
+      searchField: value,
+    }, () => {
+      onSearchChange(searchField);
+    });
+  };
+
+  render() {
     return (
       <div>
         <h1>Search Box</h1>
-        <p>{searchField}</p>
+        <AiOutlineSearch />
+        <input onChange={this.onSearchChange} />
       </div>
     );
   }
 }
+
+SearchBox.propTypes = {
+  onSearchChange: PropTypes.func.isRequired,
+};
 
 export default SearchBox;
