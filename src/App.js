e5d2a52bed4dcb6b9c829d68e940aec491c2303e
Nabil <hayounn.etd@gmail.com>
2023-02-13 14:16:16 +0100

    Create main component for quesrying cities

--- src/App.js
+++ src/App.js
@@ -1,33 +1,122 @@
 import React, { Component } from 'react';
-import SearchBox from './components/search-input/search-box.component';
+import { AiOutlineSearch } from 'react-icons/ai';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       cityName: '',
+      weatherObj: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const apiKey = '190a729e56399683fb4a81428b63841f';
-    const { cityName } = this.state;
-    if (prevState.cityName !== cityName && cityName !== '') {
-      fetch()
-    }
+  componentDidMount() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=190a729e56399683fb4a81428b63841f')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          weatherObj: data,
+        });
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  handleDataChange = (data) => {
+  handleChange = (e) => {
+    const { value } = e.target;
     this.setState({
-      cityName: data,
+      cityName: value,
     });
   }
 
+  handleClick = () => {
+    const { cityName } = this.state;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=190a729e56399683fb4a81428b63841f`)
+      .then((response) => response.json())
+      .then((data) => this.setState({
+        weatherObj: data,
+      }))
+      .catch((error) => console.log(error));
+  }
+
   render() {
+    const { weatherObj } = this.state;
+
+    const { handleChange, handleClick } = this;
+
+    if (!weatherObj) {
+      return <p>...loading</p>;
+    }
+
+    const {
+      weather: [{ description: weatherDescription }],
+      main: {
+        temp: temperature,
+        feels_like: feelsLike,
+        temp_min: tempMin,
+        temp_max: tempMax,
+        pressure,
+        humidity,
+      },
+      wind: {
+        speed,
+      },
+      name: city,
+    } = weatherObj;
     return (
       <div className="App">
-        <SearchBox onSearchChange={handleDataChange} />
+        <input onChange={handleChange} />
+        <AiOutlineSearch onClick={handleClick} />
+        <p>
+          City:
+          {' '}
+          {city}
+        </p>
+        <p>
+          Weather:
+          {' '}
+          {weatherDescription}
+        </p>
+        <p>
+          Temperature:
+          {' '}
+          {temperature}
+        </p>
+        <p>
+          Feels like:
+          {' '}
+          {feelsLike}
+        </p>
+        <p>
+          Minimum temperature:
+          {' '}
+          {tempMin}
+        </p>
+        <p>
+          Maximum temperature:
+          {' '}
+          {tempMax}
+        </p>
+        <p>
+          Wind speed:
+          {' '}
+          {speed}
+        </p>
+        <p>
+          Pressure:
+          {' '}
+          {pressure}
+        </p>
+        <p>
+          Humidity:
+          {' '}
+          {humidity}
+        </p>
       </div>
     );
   }
