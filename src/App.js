5cc57cb4e2864737f78bb944fd025d87953d4dbf
Nabil <hayounn.etd@gmail.com>
2023-02-14 09:00:23 +0100

    Update prop validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SeachInput from './components/search-input/search-input.component';
+import WeatherInfo from './components/weather-info/weather-info.component';
 import './App.css';
 
 class App extends Component {
@@ -53,74 +54,10 @@ class App extends Component {
       return <p>...loading</p>;
     }
 
-    const {
-      weather: [{ description: weatherDescription }],
-      main: {
-        temp: temperature,
-        feels_like: feelsLike,
-        temp_min: tempMin,
-        temp_max: tempMax,
-        pressure,
-        humidity,
-      },
-      wind: {
-        speed,
-      },
-      name: city,
-      sys: {
-        country,
-      },
-    } = weatherObj;
     return (
       <div className="App">
         <SeachInput onChange={handleChange} onClick={handleClick} />
-        <p>
-          City:
-          {' '}
-          {city}
-          {' '}
-          {country}
-        </p>
-        <p>
-          Weather:
-          {' '}
-          {weatherDescription}
-        </p>
-        <p>
-          Temperature:
-          {' '}
-          {temperature}
-        </p>
-        <p>
-          Feels like:
-          {' '}
-          {feelsLike}
-        </p>
-        <p>
-          Minimum temperature:
-          {' '}
-          {tempMin}
-        </p>
-        <p>
-          Maximum temperature:
-          {' '}
-          {tempMax}
-        </p>
-        <p>
-          Wind speed:
-          {' '}
-          {speed}
-        </p>
-        <p>
-          Pressure:
-          {' '}
-          {pressure}
-        </p>
-        <p>
-          Humidity:
-          {' '}
-          {humidity}
-        </p>
+        <WeatherInfo weatherObj={weatherObj} />
       </div>
     );
   }
